b6d78ab8187ec8e5da71294a2bb44e6620c138c3
Neji <newtonombese1@gmail.com>
2024-09-29 13:14:58 +0300

    Learning on the union types

---
+++ src/union.ts
@@ -0,0 +1,7 @@
+let tax: number | string = 10;
+tax = 10;
+tax = "$10";
+
+let requestStatus: "pending" | "success" | "error" = "pending";
+
+// Union types help us setup multiple types for the same variable
