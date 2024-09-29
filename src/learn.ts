6dd58cba9b197d57a4fbaefb1357c4c85672c401
Neji <newtonombese1@gmail.com>
2024-09-29 13:08:32 +0300

    Learning variable and type declarations

--- src/learn.ts
+++ src/learn.ts
@@ -1,1 +1,12 @@
-console.log('Learning Typescript')
+//  Type annotations - Allow a user to specify the variable type
+let awesomeName:string = 'shakeAndBake'
+
+let names: string = 'Newton Jillian'
+names = names.toUpperCase()
+
+let age: number = 15
+age = age + 5;
+
+let isAdult: boolean = age >= 18;
+isAdult = !isAdult;
+
