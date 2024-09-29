86c39c87c33e4435907751dd4340476a637f4c6d
Neji <newtonombese1@gmail.com>
2024-09-29 13:18:03 +0300

    Learning type any

---
+++ src/any.ts
@@ -0,0 +1,5 @@
+let notSure: any = 4;
+notSure = false;
+notSure = 'One day I believe God will lift me even more higher that my enemies can acknowledge how great He is.'
+
+// Any allows a variable to be of any type
