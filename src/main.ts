34c2e1fe93addcf534000c649ff17dedfec1e8b3
Neji <newtonombese1@gmail.com>
2024-09-30 07:40:13 +0300

    Import type annotation for ease in seeing the Devs tool console log

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 import './learn.ts'
+import './typeAnnotation.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
