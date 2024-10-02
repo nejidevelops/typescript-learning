fe566dc099b6c80fb6bb0a85500143723740e380
Neji <newtonombese1@gmail.com>
2024-10-02 15:55:42 +0300

    Learning functions in TypeScript

---
+++ src/functions.ts
@@ -0,0 +1,10 @@
+function sayHi(name: string) {
+  console.log(`Hi ${name}`);
+}
+
+sayHi('Neji')
+// sayHi(3) Does not work because name parameter is expected to be string
+
+//  any 
+// config
+// set up variable in proper type
